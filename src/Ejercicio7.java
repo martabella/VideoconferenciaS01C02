3c0fa71d0c0afc58574f0227130e44db2abd101f
martabella <mbella@lasalle.cat>
2026-02-11 20:12:58 +0100

    Añado Ejercicio 7-Tarea 1

---
+++ src/Ejercicio7.java
@@ -0,0 +1,37 @@
+/*
+Escribe el código Java que permita calcular el PVP (precio de venta al público)
+ de un producto cuando el usuario introduzca su precio base.
+ El PVP se calcula teniendo en cuenta el IVA (supondremos 21%) según la fórmula:
+PVP= 1.21*precio base
+Se aplicará un descuento del 10% al PVP del producto si su valor es igual
+o superior a 50 euros.
+
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        final float IVA=1.21f;
+        final float DESCUENTO=10f;
+
+        System.out.println("Introduce el precio base");
+        float precioBase = input.nextFloat();
+        input.nextLine();
+        if (precioBase>0) {
+            float pvp = precioBase * IVA;
+            if (pvp>=50){
+                System.out.println("Aplicando descuento "+DESCUENTO+"% de "+(precioBase*(DESCUENTO/100)));
+                float precioConDescuento = precioBase - (precioBase*(DESCUENTO/100));
+                pvp =  precioConDescuento*IVA;
+                System.out.println("El pvp es " + pvp+ " €");
+            }else {
+                System.out.println("El pvp es " + pvp + " €");
+            }
+        }else{
+            System.out.println("El valor introducido debe ser mayor que 0");
+        }
+
+    }
+}
